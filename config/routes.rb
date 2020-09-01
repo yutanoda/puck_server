651bf53d5e3f569830ae566ef70be28a93dd5838
yutanoda <atuyuta902@yahoo.co.jp>
2020-09-01 11:58:26 +0900

    uninstall devise, modify dockerfile as ruby-version

--- config/routes.rb
+++ config/routes.rb
@@ -2,5 +2,5 @@ Rails.application.routes.draw do
   root to: 'user_sessions#index'
   resources :user_sessions, only: [:index, :create, :destroy]
   resources :users
-  # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+  
 end
--- spec/controllers/users_controller_spec.rb
+++ spec/controllers/users_controller_spec.rb
@@ -4,10 +4,10 @@ include UserSessionsHelper
 RSpec.describe UsersController, type: :controller do
 
   describe "GET #index" do
-    context "as valid user" do
-      before do
-        @user = FactoryBot.create(:user)
-      end
+    before do
+      @user = FactoryBot.create(:user)
+    end
+    context "log-in-process" do
       it "returns a 200 response" do
         log_in @user
         get :index
@@ -15,13 +15,18 @@ RSpec.describe UsersController, type: :controller do
       end
     end
 
-    context "as invalid user" do
-      before do
-        @user = FactoryBot.create(:user)
-      end
+    context "as a user haven't logged in" do
       it "returns a 200 response" do
         get :index
         expect(response).to redirect_to "http://test.host/"
+      end
+    end
+
+    context "as a user logged in" do
+      it "returns a 200 response" do
+        remember @user
+        get :index
+        expect(response).to have_http_status "200"
       end
     end
   end
